8c710e643194afddc40d0ba7d45ff2f8c860cab6
Lucas Dixon (iislucas) <ldixon@google.com>
2025-08-20 13:01:28 +0300

    feat: make view style match the edit form style

--- src/app/member-view/member-view.ts
+++ src/app/member-view/member-view.ts
@@ -2,11 +2,12 @@ import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Member } from '../member.model';
 import { environment } from '../../environments/environment';
+import { IconComponent } from '../icons/icon.component';
 
 @Component({
   selector: 'app-member-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IconComponent],
   templateUrl: './member-view.html',
   styleUrls: ['./member-view.scss'],
 })
