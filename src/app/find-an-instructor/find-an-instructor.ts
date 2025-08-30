de96da7788a912f3ec37264ba1b9c46924352636
Lucas Dixon (iislucas) <ldixon@google.com>
2025-08-30 10:58:57 +0200

    fix: spinner while loading, instead of no instructors

--- src/app/find-an-instructor/find-an-instructor.ts
+++ src/app/find-an-instructor/find-an-instructor.ts
@@ -11,18 +11,25 @@ import { CommonModule } from '@angular/common';
 import { IconComponent } from '../icons/icon.component';
 import { InstructorCardComponent } from '../instructor-card/instructor-card';
 import { FindInstructorsService } from '../find-instructors.service';
+import { SpinnerComponent } from '../spinner/spinner.component';
 
 @Component({
   selector: 'app-find-an-instructor',
   standalone: true,
-  imports: [FormsModule, CommonModule, IconComponent, InstructorCardComponent],
+  imports: [
+    FormsModule,
+    CommonModule,
+    IconComponent,
+    InstructorCardComponent,
+    SpinnerComponent,
+  ],
   templateUrl: './find-an-instructor.html',
   styleUrl: './find-an-instructor.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class FindAnInstructorComponent {
-  private findInstructorsService = inject(FindInstructorsService);
+  findInstructorsService = inject(FindInstructorsService);
   searchTerm = signal('');
 
   filteredInstructors = computed(() => {
