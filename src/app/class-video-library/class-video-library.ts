e374f6f631ee0b141b553dc8ce9af1164db07de7
Lucas Dixon (iislucas) <ldixon@google.com>
2026-02-22 16:57:39 +0100

    fix: link for on demand video subscription.

--- src/app/class-video-library/class-video-library.ts
+++ src/app/class-video-library/class-video-library.ts
@@ -19,6 +19,6 @@ export class ClassVideoLibraryComponent {
   }
 
   openVideoLibrarySubscription() {
-    window.open('https://iliqchuan.com/shop/monthly-class-video-library-subscription/', '_blank');
+    window.open('https://iliqchuan.com/product/video-library-subscriber/', '_blank');
   }
 }
